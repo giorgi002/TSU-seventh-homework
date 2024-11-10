f29f3de0eed9b3aada4ea1d250b14f77eea9a331
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-11-10 20:11:09 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,802 @@
+// first task👇(1)
+// #include <iostream>
+// using namespace std;
+// int func(int x);
+// int main(){
+//     int a,result;
+//     cout << "შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის " << result << endl;
+// }
+// int func(int x){
+//     return x*x*x-4*x*x+5*x+7;
+// }
+
+
+
+// first task👇(2)
+// #include <iostream>
+// using namespace std;
+// int sum(int x){
+//     return x*x*0.5+2.5*x+12;
+// }
+// int main(){
+//     int a,result;
+//     cout << "შემოიტანეთ მთელი რიცხვი\n";
+//     cin >> a;
+//     result = sum(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// second task👇
+// #include <iostream>
+// using namespace std;
+// int func(int x){
+//     if(x>=10) return  2*x+34;
+//     else return x*x-x-1;
+// }
+// int main(){
+//     int a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin >>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// third task👇(1)
+// #include<iostream>
+// using namespace std;
+// int func(int x){
+//     if(x>3) return 5*x*x*x-12;
+//     else return x*x+2*x+4;
+// }
+// int main(){
+//     int a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// third task👇(2)
+// #include <iostream>
+// using namespace std;
+// int func(int x){
+//     if(x>=10) return 0.2*x+345;
+//     else return x*x-x-1;
+// }
+// int main(){
+//     int a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// third task👇(3)
+// #include <iostream>
+// using namespace std;
+// int func(int x){
+//     if(x>=1) return 7*x*x+3;
+//     else if(x==0) return x-25;
+//     else if(x<=-1) return x*x-6*x+8;
+// }
+// int main(){
+//     int a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// third task👇(4)
+// #include <iostream>
+// using namespace std;
+// double func(double x){
+//     if(x>5) return 11.4*x-0.3;
+//     else if(x==5) return x*x-25*x+9;
+//     else if(x<5) return x*x*x-8;
+// }
+// int main(){
+//     double a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// fourth task 👇 (1)
+// #include <iostream>
+// using namespace std;
+// int func(double x){
+//     if(x <=20.5) return 100.5*x*x+50;
+//     else return 20*x-45.5;
+// }
+// int main(){
+//     double a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// fourth task 👇 (2)
+// #include <iostream>
+// using namespace std;
+// double func(double x){
+//     if(x >10.5) return 100.5*x*x+50;
+//     else if(x==10.5) return x*x-40*x+50;
+//     else if(x<10.5) return  x+25.5;
+// }
+// int main(){
+//     double a,result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << "გამოსახულება უდრის "<< result<< endl;
+// }
+
+
+
+// fifth task 👇(1)
+// #include <iostream>
+// using namespace std;
+// string func(int x){
+//     if(x<0) return "ეს რიცხვი არის უარყოფითი";
+//     else return "ეს რიცხვი არის დადებითი";
+// }
+// int main(){
+//     int a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(2)
+// #include <iostream>
+// using namespace std;
+// string func(int x){
+//     if(x%5==0 && x%7==0) return "ეს რიცხვი არის 5 ის და 7 ის ჯერადი ";
+//     else if(x%7==0) return "ეს რიცხვი არის 7 ის ჯერადი ";
+//     else if(x%5==0) return "ეს რიცხვი არის 5 ის ჯერადი ";
+//     else return "ეს რიცხვი არ არის არც 5 ის და არც 7 ის ჯერადი ";
+// }
+// int main(){
+//     int a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(3)
+// #include <iostream>
+// using namespace std;
+// string func(double x){
+//     if(x<10) return "ეს რიცხვი არის 10 ზე ნაკლები ";
+//     else return "ეს რიცხვი არის 10 ზე მეტი  ";
+// }
+// int main(){
+//     double a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(4)
+// #include <iostream>
+// using namespace std;
+// string func(int x){
+//     if(x%10==3) return "ეს რიცხვი არის 3 ის ჯერადი ";
+//     if(x%10==9) return "ეს რიცხვი არის 9 ის ჯერადი ";
+//     else return "ეს რიცხვი არ არის არც 3 ის და არც 9 ის ჯერადი ";
+// }
+// int main(){
+//     int a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(5)
+// #include <iostream>
+// using namespace std;
+// string func(char b){
+//     if(b >='a'&& b<='z') return "ეს არ  არის პატარა ლათინური ასო";
+//     else return  "ეს არ არის ლათინური ასო";
+// }
+// int main(){
+//     char a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(6)
+// #include <iostream>
+// using namespace std;
+// string func(int b){
+//     if(b >=100&& b<=999) return "ეს არის სამნიშნა რიცხვი ";
+//     else return  "ეს არ არის სამნიშნა რიცხვი";
+// }
+// int main(){
+//     int a;
+//     string result;
+//     cout <<"შემოიტანეთ მთელი რიცხვი\n";
+//     cin>>a;
+//     result = func(a);
+//     cout << result<< endl;
+// }
+
+
+
+// fifth task 👇(7)
+// #include <iostream>
+// using namespace std;
+// bool isfirstc(char p){
+//     return(p=='c'||p=='C');
+// }
+// int main(){
+//     string str;
+//     cout << "შემოიტანეთ ტექსტი\n";
+//     cin >> str;
+//     if(isfirstc(str[0])){
+//         cout << "სიტყვა იწყება c ასოთი\n";
+//     }
+//     else{
+//         cout << "სიტყვა არ იწყება c ასოთი\n";
+//     }
+// }
+
+
+
+// fifth task 👇(8)
+// #include <iostream>
+// using namespace std;
+// bool isfirstp(char p){
+//     return(p=='p' ||p=='P');
+// }
+// bool islasth(char x){
+//     return(x=='h'|| x=='H');
+// }
+// int main(){
+//     string str;
+//     cout << "შემოიტანეთ ტექსტი\n";
+//     cin >> str;
+//     if(isfirstp(str[0])){
+//         cout << "სიტყვა იწყება p ასოთი\n";
+//     }
+//     else{
+//         cout << "სიტყვა არ იწყება p ასოთი\n";
+//     }
+//     if(islasth(str[str.length()-1])){
+//         cout <<"სიტყვა მთავრდება h ასოთი\n";
+//     }
+//     else{
+//         cout << "სიტყვა არ მთავრდება h ასოთი\n";
+//     }
+// }
+
+
+
+// sixth task 👇(1)
+// #include <iostream>
+// using namespace std;
+// int func(int x,int y){
+//     int sum =0;
+//     int counter=0;
+//     for(int i=x; i<=y;i++){
+//         if(i%2==1){
+//             sum+=i;
+//             counter++;
+//         }
+//     }
+//     return sum/counter;
+// }
+// int main(){
+//     int x,y;
+//     cout << "შემოიტანეთ რიცხვთა შუალედი\n";
+//     cin>>x >>y;
+//     int result = func(x,y);
+//     cout << result<<endl;
+// }
+
+
+
+// sixth task 👇 (2)
+// #include <iostream>
+// using namespace std;
+// int func(int x,int y){
+//     int counter=0;
+//     for(int i =x;i<=y;i++){
+//         if(i%2==0){
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+// int main(){
+//     int x,y,result;
+//     cout << "შემოიტანეთ მთელი რიცხვების შუალედი\n";
+//     cin>>x>>y;
+//     result =func(x,y);
+//     cout <<"ამ შუალედში არის "<< result<<" ცალი ლუწი რიცხვი"<<endl;
+// }
+
+
+
+// sixth task 👇(3)
+// #include <iostream>
+// using namespace std;
+// int func(int a,int b){
+//     int sum=0;
+//     int counter=0;
+//     for(int i=a;i<=b;i++){
+//         if(i%7==0){
+//             counter++;
+//             sum+=i;
+//         }
+//     }
+//     return sum;
+// }
+// int main(){
+//     int a,b,result;
+//     cout << "შემოიტანეთ მთელი რიცხვების შუალედი\n";
+//     cin >>a>>b;
+//     result = func(a,b);
+//     cout <<"ამ შუალედში შვიდის ჯერადი რიცხვების ჯამი არის " << result<< " ის ტოლი" << endl;
+// }
+
+
+
+// sixth task 👇(4)
+// #include <iostream>
+// using namespace std;
+// int func(int k, int m){
+//     int sum=0;
+//     int counter=0;
+//     for(int i=k;i<=m;i++){
+//         if(i%3==0){
+//             counter++;
+//             sum+=i;
+//         }
+//     }
+//     return sum/counter;
+// }
+// int main(){
+//     int k,m,result;
+//     cout << "შემოიტანეთ მთელი რიცხვების შუალედი\n";
+//     cin >>k>>m;
+//     result = func(k,m);
+//     cout <<"ამ შუალედში სამის ჯერადი რიცხვების საშუალო არის " << result<< " ის ტოლი" << endl;
+// }
+
+
+
+// seventh task 👇(1)
+// #include <iostream>
+// using namespace std;
+// int func(int x,int y,int z){
+//     int sum=0;
+//     int counter=3;
+//     sum=x+y+z;
+//     return sum/counter;
+// }
+// int main(){
+//     int x,y,z,result;
+//     cout << "შემოიტანეთ სამი მთელი რიცხვი\n";
+//     cin >>x>>y>>z;
+//     result=func(x,y,z);
+//     cout << "ამ რიცხვების საშუალო არის "<< result << endl;
+// }
+
+
+
+// seventh task 👇(2)
+// #include <iostream>
+// using namespace std;
+// int cube(int num){
+//     return num*num*num;
+// }
+// int main(){
+//     for(int i=1;i<100;i++){
+//         cout << "რიცხვის  კუბი არის "<< cube(i)<< endl;
+//     }
+// }
+
+
+
+// seventh task 👇(3)
+// #include <iostream>
+// using namespace std;
+// int rect(int x,int y){
+//     return x*y;
+// }
+// int main(){
+//     int result = rect(5,7);
+//     cout << "მართკუთხედის ფართობი არის "<< result << endl;
+//     int result2 = rect(8,10);
+//     cout << "მეორე მართკუთხედის ფართობი არის "<< result2 << endl;
+//     if(result> result2){
+//         cout << "პირველი მართკუთხედის ფართობი მეტია ვიდრე მეორესი"<< endl;
+//     }
+//     else{
+//          cout << "მეორე მართკუთხედის ფართობი მეტია  ვიდრე პირველის"<< endl;
+//     }
+// }
+
+
+
+// seventh task 👇(4)
+// #include <iostream>
+// using namespace std;
+// int min(int x, int y){
+//     if(x >y){
+//         return y;
+//     }
+//     else{
+//         return x;
+//     }
+// }
+// int main(){
+//     int result = min(8,5);
+//     cout << "ამ რიცხვებიდან უმცირესი არის " << result << endl;
+// }
+
+
+
+// seventh task 👇(5)
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// double triangleArea(double a, double b, double c) {
+//     double s = (a + b + c) / 2;
+//     return sqrt(s * (s - a) * (s - b) * (s - c));
+// }
+// int main(){
+//     double result = triangleArea(2,3,6);
+//     double result2 = triangleArea(5,8,7);
+//     if(result > result2){
+//         cout << "პირველი სამკუთხედის ფართობი მეტია ვიდრე მეორესი "<< endl;
+//     }
+//     else{
+//         cout << "მეორე სამკუთხედის ფართობი მეტია ვიდრე პირველის "<< endl;
+//     }
+// }
+
+
+
+// seventh task 👇(6)
+// #include <iostream>
+// using namespace std;
+// bool isvowel(char x){
+//     return(x=='a'||x=='e'||x=='i'||x=='o'||x=='u');
+// }
+// int main(){
+//     string str;
+//     cout << "შემოიტანეთ ტექსტი\n";
+//     cin >> str;
+//     bool result = isvowel(str[0]);
+//     cout << result << endl;
+// }
+
+
+
+// seventh task 👇(7)
+// #include <iostream>
+// using namespace std;
+// int func(char p){
+//     if(p >='0' && p<='9') return int(p);
+//     else  return -1;
+// }
+// int main(){
+//     char p;
+//     cout << "შემოიტანეთ სიმბოლო\n";
+//     cin >> p;
+//     int result = func(p);
+//     cout <<"თქვენ მიერ დაწერილი სიმბოლო არის "<<result<< endl;
+// }
+
+
+
+// eighth task 👇
+// #include <iostream>
+// using namespace std;
+// int func(int x,int y){
+//     return( x>y)?x:y;
+// }
+// int main(){
+//     int x,y;
+//     cout << "შემოიტანეთ მთელი რიცხვები\n";
+//     cin >> x>>y;
+//     int result = func(x,y);
+//     cout << "ამ ორი რიცხვიდან მეტია "<< result << endl;
+// }
+
+
+
+// task  nine👇
+// #include <iostream>
+// using namespace std;
+// double func(double x,double y){
+//     return x<y ?x:y;
+// }
+// int main(){
+//     double x,y,result;
+//     cout << "შემოიტანეთ ორი ნამდვილი რიცხვი\n";
+//     cin >>x >>y;
+//     result = func(x,y);
+//     cout << "ამ ორი რიცხვიდან პატარაა "<< result << endl;
+// }
+
+
+
+// task ten 👇
+// #include <iostream>
+// using namespace std;
+// bool isPalindrome(int num) {
+//     int original = num;
+//     int reversed = 0;
+//     while (num > 0) {
+//         int digit = num % 10;     
+//         reversed = reversed * 10 + digit; 
+//         num /= 10;            
+//     }
+//     return original == reversed;
+// }
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     if (isPalindrome(num)) {
+//         cout << num << " is a palindrome number." << endl;
+//     } else {
+//         cout << num << " is not a palindrome number." << endl;
+//     }
+// }
+
+
+
+// task eleven 👇
+// #include <iostream>
+// using namespace std;
+// bool func(int x){
+//      return ((x*x)==x%10);
+// }
+// int main(){
+//     int x;
+//     bool result;
+//     cout << "შემოიტანეთ მთელი რიცხვი\n";
+//     cin >> x;
+//     result = func(x);
+//     if(result) {
+//         cout << "ეს რიცხვი არის ავტომორფული \n";
+//     }
+//     else{
+//         cout << "ეს რიცხვი არის ავტომორფული"<< endl;
+//     }
+// }
+
+
+
+// task twelve 👇
+// #include <iostream>
+// using namespace std;
+// bool func(int x){
+//     int sum=0;
+//     for(int i =1; i<=x/2;i++){
+//         if(x%i==0){
+//             sum +=i;
+//         }
+//     }
+// return (sum ==x);
+// }
+// int main(){
+//     int x;
+//     bool result;
+//     cout << "შემოიტანეთ რიცხვი\n";
+//     cin >>x;
+//     result = func(x);
+//     if(result) cout << "ეს რიცხვი არი სრულყოფილი\n";
+//     else{
+//         cout << "ეს რიცხვი არ არის სრულყოფილი\n";
+//     }
+// }
+
+
+
+// task thirteen 👇
+// #include <iostream>
+// using namespace std;
+// bool func( int x){
+//     if(x==1) return false;
+//     else return (x %1==0 && x%x==0);
+// }
+// int main(){
+//     int x;
+//     bool result;
+//     cout << "შემოიტანეთ მთელი რიცხვი\n";
+//     cin >>x;
+//     result = func(x);
+//     if(result) cout << "ეს რიცხვი არის მარტივი\n";
+//     else{
+//         cout << "ეს რიცხვი არ არის მარტივი\n";
+//     }
+// }
+
+
+
+// task fourteen 👇
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// void dr(int m){
+//     for(int i =0;i <m;i++) {
+//         cout <<"*";
+//         cout <<"+";
+//         cout <<"<";
+//         cout <<"/";
+//     }
+//     cout << endl;
+// }
+// int main(){
+//     int x;
+//     cout << "შემოიტანეთ მთელი რიცხვი\n";
+//     cin >>x;
+//     dr(x);
+// }
+
+
+
+// task fifteen 👇
+// #include <iostream>
+// #include <random>
+// #include <ctime>
+// #include <algorithm>
+// using namespace std;
+// void printChars(const vector<short> x) {
+// 	for (const auto value : x)
+// 		cout << (char)value << ' ';
+// 	cout << endl;
+// }
+// int main() 
+// {
+// 	vector<short> codes;
+// 	default_random_engine dre(time(NULL));	 
+// 	uniform_int_distribution<short> dis('a', 'z');
+// 	for (int count =1 ; count <= 10; count++)
+// 		  codes.push_back( dis(dre) );
+// 	printChars(codes);
+// 	sort(codes.begin(), codes.end());
+// 	printChars(codes);
+// 	cout << "umciresia " << char(codes[0]) << " kodiT " << codes[0] << endl;
+// 	cout << "udidesia " << char(codes[ codes.size() - 1] )
+// 	     << " kodiT " << codes[ codes.size() - 1 ] << endl;
+// }
+
+
+
+// task sixteen 👇
+// #include <iostream> 
+// #include <vector>
+// #include <fstream>
+// using namespace std;
+// int maxIndex(const vector<double> );
+// int main()
+// {
+//     vector<double> vec;
+//     double x;
+//     ifstream ifs("data.txt");
+//     while (ifs >> x)
+//         vec.push_back(x);
+//     if (vec.size() > 0)	
+//     { 
+// 	 int index = maxIndex(vec);
+// 	 cout << "Index of maximum element is " << index 
+// 	      << "\n maximum element itself = " << vec[index] << endl;
+//     }    
+//     else cout << "Vector is empty!" << endl;
+// }
+// int maxIndex(const vector<double> v)
+// {
+//     int maxind(0);
+//     for (int i = 1; i < v.size(); ++i)
+//     	  if ( v[maxind] < v[i] )
+//         	maxind = i;
+//     return maxind;
+// }
+
+
+
+// task seventeen 👇
+// #include <iostream>
+// using namespace std;
+// int my_log2(int N){
+//     int d; 
+//     if( N < 0 ) N = -N;
+//     for(d = 0;  N > 0;  d++, N /= 2);
+//     return d;
+// }
+// int main(){
+//    int N, degree;
+//    cin >> N;
+//    degree = my_log2(N);
+//    cout << degree << endl;   
+// }
+
+
+
+// task eightten 👇
+// #include <iostream>
+// using namespace std;
+// double sum(){
+//     double sum=0.0;
+//     for(int i=1;i<20;i+=2){
+//         sum +=1.0/i;
+//     }
+//     return sum;
+// }
+// int main(){
+//     double result = sum();
+//     cout <<result << endl;
+// }
+
+
+
+// task nineteen 👇(1)
+// #include <iostream>
+// #include <string>
+// #include <ctime>
+// using namespace std;
+// int main(){
+//     time_t today_time =time(NULL);
+//     string dt = ctime(&today_time);
+//     cout << "the local date and time is " << dt << endl;
+// }
+
+
+
+// task nineteen 👇(2)
+// #include <iostream>
+// #include <chrono>
+// #include <ctime>
+// using namespace std;
+// int main(){
+//     auto st = chrono::system_clock::now();
+//     time_t today_time = chrono::system_clock::to_time_t(st);
+// 	cout << "today is: " << ctime(&today_time);
+// }
